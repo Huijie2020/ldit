fb27ffc170262560dcfdd8eaa1f5f9044c9445eb
huijie2020 <wszhj2008@gmail.com>
2024-04-18 21:23:52 -0700

    baseline cfg

--- train.py
+++ train.py
@@ -382,10 +382,10 @@ if __name__ == "__main__":
     parser.add_argument("--max-depth", type=float, default=80.0)
     parser.add_argument("--num-classes", type=int, default=1)
     parser.add_argument("--epochs", type=int, default=300)
-    parser.add_argument("--global-batch-size", type=int, default=100)
+    parser.add_argument("--global-batch-size", type=int, default=160)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
-    parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument("--num-workers", type=int, default=8)
     parser.add_argument("--log-every", type=int, default=100)
     parser.add_argument("--ckpt-every", type=int, default=50_000)
     args = parser.parse_args()
